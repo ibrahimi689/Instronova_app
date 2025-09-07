029d46b8ba693e2921d1883b6317927c8d118e7e
ibrahimi689 <ahmeed154ee@gmail.com>
2025-09-07 18:22:51 +0530

    Create app_assets.dart

---
+++ constants/app_assets.dart
@@ -0,0 +1,15 @@
+// app_assets.dart
+// All asset paths used in the login/signup module
+
+class AppAssets {
+  // Logo
+  static const String logo = 'assets/icons/logo.svg';
+
+  // Buttons
+  static const String loginButton = 'assets/icons/login_button.svg';
+  static const String signupButton = 'assets/icons/signup_button.svg';
+
+  
+  // Backgrounds
+  static const String loginBackground = 'assets/icons/login_background.svg';
+}
