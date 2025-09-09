907298eff586367a2108129a253f843e8029265a
ibrahimi689 <ahmeed154ee@gmail.com>
2025-09-09 14:30:22 +0530

    Create app_strings.dart

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,15 @@
+class AppStrings {
+  static const String usernameHint = "Username, email or mobile number";
+  static const String passwordHint = "Password";
+  static const String login = "Log in";
+  static const String forgotPassword = "Forgot password?";
+  static const String createAccount = "Create new account";
+  static const String signup = "Sign up";
+
+  // 🔹 OTP Strings
+  static const String enterOtp = "Enter OTP";
+  static const String otpSubtitle =
+      "We have sent a 6-digit code to your email/phone";
+  static const String verify = "Verify";
+  static const String resendOtp = "Resend OTP";
+}
